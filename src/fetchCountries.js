e4f0e241a906618a5428ec677cc32bc65676cec6
IrynaTopolna <itopolna2808@gmail.com>
2023-01-20 11:56:06 +0200

    Adds new files

---
+++ src/fetchCountries.js
@@ -0,0 +1,11 @@
+export default function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error('Oops, there is no country with that name');
+    }
+    return resp.json();
+  });
+}
